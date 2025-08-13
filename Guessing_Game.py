84827c72f6718be53297d8ecf5a9a501128d0e39
Bryan Dave <hbryandave411@gmail.com>
2025-08-13 15:06:44 +0800

    Update Guessing_Game.py

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -1,39 +1,34 @@
 # PROJECT 3: GUESSING GAME
-
 # 🎯 Number Guessing Game
-
 # HeroBoard Entry: #3 - Day 3 Project 03
 
 import random
 
 print("🎮 Welcome to the Number Guessing Game!")
-print("I'm thingking of a number betwwen 1 and 10.\n")
+print("I'm thinking of a number between 1 and 10.\n")
 
 # Secret number
-
 secret_number = random.randint(1, 10)
 guess = None
 tries = 0
 
 # Loop until correct guess
-
 while guess != secret_number:
-guess = int(input("🤔 What's your guess? "))
-tries += 1
+    guess = int(input("🤔 What's your guess? "))
+    tries += 1
 
-if guess < secret_number:
-print("🔺 Too low! Try again.\n")
-elif guess > secret_number:
-print("🔻 Too high: Try again.\n")
-else:
-print("✔ Correct! You guessed the number.")
+    if guess < secret_number:
+        print("🔺 Too low! Try again.\n")
+    elif guess > secret_number:
+        print("🔻 Too high! Try again.\n")
+    else:
+        print("✔ Correct! You guessed the number.")
 
 # Final stats
-
 print(f"\n🎉 You found it in {tries} tries!")
 if tries <= 1:
-print("🏆 One-shot kill! You're a mind reader!")
+    print("🏆 One-shot kill! You're a mind reader!")
 elif tries <= 3:
-print("💪 Sharp guesser!")
+    print("💪 Sharp guesser!")
 else:
-print("😅 That was close! But you made it!")
+    print("😅 That was close! But you made it!")
